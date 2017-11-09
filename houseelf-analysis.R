75ce4580ad772eb8e67de98e55847983ebac45fe
Robert Benjamin Lemings <robertbenjaminlemings@Roberts-MacBook-Pro.local>
2017-11-09 11:18:34 -0600

    - revised GC content function - added code to mutate gc content and earlengths - this allowed the data to be analized for a correlation between gc content and earlenghths

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,12 +1,15 @@
-house_elf <- read.csv("./data/houseelf_earlength_dna_data1.csv", header = TRUE)
-house_elf <- .72 * 4.01
+house_elf <- read.csv("./data/houseelf_earlength_dna_data.csv", header = TRUE)
+
+
 #6
-library(stringr)
-gc_content <- function(file){
-  reads <- readFasta(paste0("./data/houseelf_earlength_dna_data1/", file))
-  seq <- sread(reads)
-  Gs <- str_to_lower(seq, "G")
-  Cs <- str_to_upper(seq, "C")
+
+gc_content <- function(seq){
+  library(stringr)
+  GC <- str_to_upper(seq)
+  Gs <- str_count(GC, "G")
+  Cs <- str_count(GC, "C")
+  seq_length <- str_length(GC)
+  gc_content <- (Gs + Cs)/ seq_length * 100
   return(gc_content)
 }
 #7
@@ -15,3 +18,10 @@ get_size_class <- function(ear_length){
   ear_lengths <- ifelse(ear_length > 10, "large", "small")
   return(ear_lengths)
 }
+ 
+data_frame_ID <- 
+house_elf %>% 
+  mutate( gc_cont = gc_content(seq = dnaseq),
+        get_size = get_size_class(ear_length = earlength),
+        dnaseq = NULL,
+        earlength = NULL)
